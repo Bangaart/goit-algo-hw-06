81ec587ff0b0a3a30fa45dcd756486b3799709d1
Arthur <planck2909@gmail.com>
2025-04-20 22:10:33 +0300

    after refactoring

--- cli_programme.py
+++ cli_programme.py
@@ -2,21 +2,23 @@ from collections import UserDict
 
 class Field:
     def __init__(self, value):
+        if not self.is_valid(value):
+            raise ValueError
         self.value = value
+
+    def is_valid(self, value):
+        return True
 
     def __str__(self):
         return str(self.value)
 
 class Name(Field):
     pass
-    
+
 class Phone(Field):
 
-    def phone_validation(self):
-        if len(self.value) == 10 and self.value.isdigit():
-            return self.value
-        else:
-            raise ValueError ("Phone number should be 10 digits and don't contain alphabet symbols")
+    def is_valid(self, value):
+        return len(value) == 10 and value.isdigit()
 
 class Record:
     def __init__(self, name):
@@ -24,44 +26,50 @@ class Record:
         self.phones = []
 
     def add_phone(self, phone):
-        self.phones.append(Phone(phone).phone_validation())
+        self.phones.append(Phone(phone))
 
     def edit_phone(self, old_number, new_number):
-        if old_number in self.phones:
-            self.phones[self.phones.index(old_number)] = new_number
+        phone_obj = self.find_phone(old_number)
+        if  phone_obj:
+            self.remove_phone(old_number)
+            self.add_phone(new_number)
         else:
             raise ValueError ("Check correctness of the number ")
 
     def find_phone(self, phone):
-        if phone in self.phones:
-            return phone
+        for p in self.phones:
+            if p.value == phone:
+                return p
+        return None
 
     def remove_phone(self, phone):
-        self.phones.remove(phone)
+        phone_obj = self.find_phone(phone)
+        if phone_obj:
+            self.phones.remove(phone_obj)
 
     def __str__(self):
-        return f"Contact name: {self.name.value}, phones: {'; '.join(p for p in self.phones)}"
+        return f"Contact name: {self.name}, phones: {'; '.join(p.value for p in self.phones)}"
 
 class AddressBook(UserDict):
 
     def add_record(self, record_object):
-        self.data[str(record_object.name)] = record_object.phones
+        self.data[record_object.name.value] = record_object
 
     def find(self, name):
         if name in self.data.keys():
-            a = Record(name)
-            a.phones = self.data[name]
-            return a
-
+            return self.data.get(name)
+        return None
 
     def delete(self, name):
         del self.data[name]
 
     def __str__(self):
-        full_string = ''
-        for i in self.data:
-            full_string += f"Contact name : {i}, phones: {'; '.join(p for p in self.data[i])}\n"
-        return full_string
+        result = ""
+        for name, value in self.data.items():
+            phones = ";".join(p.value for p in value.phones)
+            result += f"Contact name : {name}, phones: {phones}\n"
+        return result.strip()
+
 
     # Створення нової адресної книги
 book = AddressBook()
@@ -86,7 +94,7 @@ print(book)
 # Знаходження та редагування телефону для John
 john = book.find("John")
 john.edit_phone("1234567890", "1112223333")
-
+john.remove_phone("1112223333")
 print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
 
 # Пошук конкретного телефону у записі John
@@ -95,4 +103,5 @@ print(f"{john.name}: {found_phone}")  # Виведення: John: 5555555555
 
 # Видалення запису Jane
 book.delete("Jane")
+
 
